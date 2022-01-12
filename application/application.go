770da3900aa3eca49360e898ca65e3cf6c7aa39d
Mathieu Lecarme <mlecarme@bearstech.com>
2022-01-12 12:44:49 +0100

    Auth.

--- application/application.go
+++ application/application.go
@@ -1,6 +1,7 @@
 package application
 
 import (
+	"github.com/factorysh/microdensity/middlewares"
 	"github.com/factorysh/microdensity/queue"
 	"github.com/factorysh/microdensity/service"
 	"github.com/go-chi/chi/v5"
@@ -13,7 +14,7 @@ type Application struct {
 	router   chi.Router
 }
 
-func New(q *queue.Queue) (*Application, error) {
+func New(q *queue.Queue, secret string) (*Application, error) {
 	r := chi.NewRouter()
 	a := &Application{
 		Services: make([]*service.Service, 0),
@@ -26,12 +27,16 @@ func New(q *queue.Queue) (*Application, error) {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
+	r.Use(middlewares.Auth(secret))
+
 	r.Get("/services", a.services)
 	r.Route("/service/{serviceID}", func(r chi.Router) {
 		r.Use(a.serviceCtx)
 		r.Get("/", a.service)
 		r.Post("/", a.newTask)
-		r.Get("/{taskID}", a.task)
+		r.Route("/{taskID}", func(r chi.Router) {
+			r.Get("/", a.task)
+		})
 	})
 
 	return a, nil
--- application/application_test.go
+++ application/application_test.go
@@ -5,14 +5,18 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
+	"github.com/cristalhq/jwt/v3"
+	"github.com/factorysh/microdensity/claims"
 	"github.com/factorysh/microdensity/service"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestApplication(t *testing.T) {
-	a, err := New(nil)
+	secret := "s3cr37"
+	a, err := New(nil, secret)
 	assert.NoError(t, err)
 	a.Services = append(a.Services, &service.Service{
 		Name: "demo",
@@ -21,15 +25,57 @@ func TestApplication(t *testing.T) {
 	ts := httptest.NewServer(a.router)
 	defer ts.Close()
 
-	r, err := http.Get(fmt.Sprintf("%s/services", ts.URL))
+	cli := http.Client{}
+
+	req, err := mkRequest(secret)
 	assert.NoError(t, err)
+	req.Method = http.MethodGet
+	req.URL, err = url.Parse(fmt.Sprintf("%s/services", ts.URL))
+	assert.NoError(t, err)
+
+	r, err := cli.Do(req)
+	assert.NoError(t, err)
+	assert.Equal(t, 200, r.StatusCode)
 	dec := json.NewDecoder(r.Body)
 	var services []string
 	err = dec.Decode(&services)
 	assert.NoError(t, err)
 	assert.Equal(t, []string{"demo"}, services)
 
-	r, err = http.Get(fmt.Sprintf("%s/service/demo", ts.URL))
+	req, err = mkRequest(secret)
+	assert.NoError(t, err)
+	req.Method = http.MethodGet
+	req.URL, err = url.Parse(fmt.Sprintf("%s/service/demo", ts.URL))
+	assert.NoError(t, err)
+
+	r, err = cli.Do(req)
 	assert.NoError(t, err)
 	assert.Equal(t, 200, r.StatusCode)
+}
+
+func freshToken(key string) (*jwt.Token, error) {
+	signer, err := jwt.NewSignerHS(jwt.HS256, []byte(key))
+	if err != nil {
+		return nil, err
+	}
+	clm := claims.Claims{
+		Owner: "Bob",
+		Admin: false,
+		Path:  "group/project",
+	}
+
+	builder := jwt.NewBuilder(signer)
+	return builder.Build(clm)
+}
+
+func mkRequest(key string) (*http.Request, error) {
+	t, err := freshToken(key)
+	if err != nil {
+		return nil, err
+	}
+	return &http.Request{
+		Header: http.Header{
+			"Authorization": {fmt.Sprintf("Bearer %s", t.String())},
+		},
+	}, nil
 }
