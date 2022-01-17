77b9881c4447f7aeb317b6f0738d0f46483c4a84
Mathieu Lecarme <mlecarme@bearstech.com>
2022-01-17 21:30:27 +0100

    Service is an interface

--- service/service.go
+++ service/service.go
@@ -1,5 +1,12 @@
 package service
 
-type Service struct {
-	Name string
+type Service interface {
+	// Validate the input coming from HTTP Body as a JSON, and fight against XSS
+	//Validate is sync
+	Validate(map[string]interface{}) error
+	// When the queue is ok, run async
+	// Run can use the user's Sentry ID
+	Run(args map[string]interface{}) error
+	// What's service name?
+	Name() string
 }
