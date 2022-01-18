18f16ba1176207dcd5a6428696ed4eb301981051
Mathieu Lecarme <mlecarme@bearstech.com>
2022-01-18 22:03:01 +0100

    More up.

--- run/compose.go
+++ run/compose.go
@@ -6,14 +6,59 @@ import (
 	"os"
 	"path"
 
-	"github.com/docker/cli/cli/compose/loader"
+	"github.com/compose-spec/compose-go/loader"
+	"github.com/compose-spec/compose-go/types"
+	"github.com/docker/cli/cli/config/configfile"
+	"github.com/docker/compose/v2/pkg/api"
+	"github.com/docker/compose/v2/pkg/compose"
+	"github.com/docker/docker/client"
+	"golang.org/x/net/context"
 )
 
 type ComposeRun struct {
-	home string
+	home    string
+	details types.ConfigDetails
+	service api.Service
+}
+
+func dockerConfig() (*configfile.ConfigFile, error) {
+	pth := path.Join(os.Getenv("HOME"), "/.docker/config.json")
+	_, err := os.Stat(pth)
+	dockercfg := &configfile.ConfigFile{}
+	if err != nil {
+		if os.IsNotExist(err) {
+			f, err := os.Open(pth)
+			if err != nil {
+				return nil, err
+			}
+			defer f.Close()
+			err = dockercfg.LoadFromReader(f)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			return nil, err
+		}
+	} else {
+		dockercfg = configfile.New(pth)
+	}
+	return dockercfg, nil
 }
 
 func NewComposeRun(home string) (*ComposeRun, error) {
+	dockercfg, err := dockerConfig()
+	if err != nil {
+		return nil, err
+	}
+	docker := &client.Client{}
+	err = client.FromEnv(docker)
+	if err != nil {
+		return nil, err
+	}
+
+	service := compose.NewComposeService(docker, dockercfg)
+	fmt.Println(service)
+
 	cfg, err := os.Open(path.Join(home, "docker-compose.yml"))
 	if err != nil {
 		return nil, err
@@ -24,16 +69,36 @@ func NewComposeRun(home string) (*ComposeRun, error) {
 		return nil, err
 	}
 	yml, err := loader.ParseYAML(raw)
-	fmt.Println(yml)
-
 	if err != nil {
 		return nil, err
 	}
-	return &ComposeRun{
-		home: home,
-	}, nil
+	fmt.Println(yml)
+
+	details := types.ConfigDetails{
+		WorkingDir: home,
+		ConfigFiles: []types.ConfigFile{
+			{
+				Filename: home,
+				Config:   yml,
+			},
+		},
+		Environment: map[string]string{},
+	}
+
+	cr := &ComposeRun{
+		home:    home,
+		details: details,
+		service: service,
+	}
+
+	return cr, nil
 }
 
-func (c *ComposeRun) Run(args map[string]interface{}) {
+func (c *ComposeRun) Run(ctx context.Context, args map[string]interface{}) error {
+	project, err := loader.Load(c.details)
+	if err != nil {
+		return err
+	}
 
+	return c.service.Up(ctx, project, api.UpOptions{})
 }
---
+++ run/compose_test.go
@@ -0,0 +1,14 @@
+package run
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCompose(t *testing.T) {
+	cr, err := NewComposeRun("demo/")
+	assert.NoError(t, err)
+	fmt.Println(cr)
+}
