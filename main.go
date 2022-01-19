d421846c8f44e7d3f2e3ca659e37eedf53092479
Wilfried OLLIVIER <wollivier@bearstech.com>
2022-01-19 18:33:12 +0100

    Main app use oauth package

--- main.go
+++ main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/factorysh/microdensity/middlewares"
+	"github.com/factorysh/microdensity/oauth"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	// get OAuth config from env
+	oauthConfig, err := oauth.NewConfigFromEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// routing and handlers
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+
+	// protected routes
+	r.Group(func(r chi.Router) {
+		r.Use(middlewares.OAuth(oauthConfig))
+		r.Get("/protected", func(w http.ResponseWriter, _ *http.Request) {
+			w.Write([]byte("i am protected"))
+			w.WriteHeader(http.StatusOK)
+		})
+	})
+	// oauth callback hander on /oauth/callback
+	r.Get(oauth.CallbackEndpoint, oauth.CallbackHandler(oauthConfig))
+
+	r.Get("/", func(w http.ResponseWriter, _ *http.Request) {
 		w.Write([]byte("welcome"))
 	})
+
 	http.ListenAndServe(":3000", r)
 }
