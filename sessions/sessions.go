e0e6f1e4f971a4c8830e5f99accae3cbdaea442d
Wilfried OLLIVIER <wollivier@bearstech.com>
2022-01-20 19:05:57 +0100

    Add methods to sessions pkg

--- sessions/sessions.go
+++ sessions/sessions.go
@@ -1,6 +1,7 @@
 package sessions
 
 import (
+	"net/url"
 	"sync"
 	"time"
 
@@ -13,10 +14,39 @@ type UserData struct {
 	Project *gitlab.ProjectInfo
 }
 
+// IsValid is used to check is user data is expired
+func (ud *UserData) IsValid() bool {
+	return ud.Expires.After(time.Now())
+}
+
+// MatchRequestedProject check is this user session match the required project
+func (ud *UserData) MatchRequestedProject(requestedProject string) bool {
+	requestedProject, err := url.PathUnescape(requestedProject)
+	if err != nil {
+		return false
+	}
+
+	// TODO : handle both ID and full project name
+
+	return ud.Project.PathWithNamespace == requestedProject
+}
+
 // Sessions pool
 type Sessions struct {
 	sync.RWMutex
 	pool map[string]*UserData
+}
+
+// Authorize user to access to matching ressource
+func (s *Sessions) Authorize(accessToken string, projectName string) bool {
+	s.RLock()
+	ud, found := s.pool[accessToken]
+	s.RUnlock()
+	if !found {
+		return false
+	}
+
+	return ud.IsValid() && ud.MatchRequestedProject(projectName)
 }
 
 // Put UserData into session pool
@@ -28,6 +58,24 @@ func (s *Sessions) Put(accessToken string, expires time.Time, project *gitlab.Pr
 	}
 	s.Unlock()
 }
+
+// Get UserData from session pool
+func (s *Sessions) Get(accessToken string) (*UserData, bool) {
+	s.RLock()
+	u, found := s.pool[accessToken]
+	s.RUnlock()
+
+	return u, found
+}
+
+// Remove UserData from session pool
+func (s *Sessions) Remove(accessToken string) {
+	s.Lock()
+	delete(s.pool, accessToken)
+	s.Unlock()
+}
+
+// TODO: prune sessions
 
 // New inits a new sessions struct
 func New() Sessions {
