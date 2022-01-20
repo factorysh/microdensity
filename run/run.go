cbdb479a2260cc10d1d4d5f7b28399a4676e618e
Mathieu Lecarme <mlecarme@bearstech.com>
2022-01-20 16:51:31 +0100

    Got an interface.

--- run/run.go
+++ run/run.go
@@ -1,1 +1,11 @@
 package run
+
+import (
+	"io"
+
+	"golang.org/x/net/context"
+)
+
+type Run interface {
+	Run(ctx context.Context, args map[string]string, stdout io.WriteCloser, stderr io.WriteCloser) (int, error)
+}
