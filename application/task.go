8e54b1d8c5f0859370196733feeb00da022adeb5
Mathieu Lecarme <mlecarme@bearstech.com>
2022-01-26 15:42:53 +0100

    Validate

--- application/task.go
+++ application/task.go
@@ -2,10 +2,12 @@ package application
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/factorysh/microdensity/httpcontext"
+	_service "github.com/factorysh/microdensity/service"
 	"github.com/factorysh/microdensity/task"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
@@ -21,11 +23,20 @@ func (a *Application) newTask(w http.ResponseWriter, r *http.Request) {
 	}
 	if project != jwtProject {
 		w.WriteHeader(403)
+		return
 	}
 	var args map[string]interface{}
+
 	err = render.DecodeJSON(r.Body, args)
 	if err != nil {
 		panic(err)
+	}
+	s := r.Context().Value("service").(_service.Service)
+	err = s.Validate(args)
+	if err != nil {
+		w.WriteHeader(400)
+		fmt.Fprintln(w, err)
+		return
 	}
 	id, err := uuid.NewUUID()
 	if err != nil {
