05292a05f1b4719870769eb0144dec2f3bd6e8c5
Mathieu Lecarme <mlecarme@bearstech.com>
2022-01-29 00:00:48 +0100

    More JWT.

--- middlewares/jwt.go
+++ middlewares/jwt.go
@@ -11,6 +11,61 @@ import (
 	"github.com/getsentry/sentry-go"
 )
 
+type JWKS struct {
+	Keys []struct {
+		Kty      string `json:"kty"`
+		Kid      string `json:"kid"`
+		E        string `json:"e"`
+		N        string `json:"n"`
+		Use      string `json:"use"`
+		Alg      string `json:"alg"`
+		verifier jwt.Verifier
+	} `json:"keys"`
+}
+
+func NewJWTAuthenticator(gitlab string) (*JWKS, error) {
+	r, err := http.Get(fmt.Sprintf("https://%s/-/jwks", gitlab))
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+	var j JWKS
+	err = json.NewDecoder(r.Body).Decode(&j)
+	if err != nil {
+		return nil, err
+	}
+	for _, k := range j.Keys {
+		k.verifier, err = jwt.NewVerifierHS(jwt.Algorithm(k.Alg), []byte(k.N))
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &j, err
+}
+
+func (j *JWKS) ParseAndValidate(jwtRaw string) (*jwt.Token, error) {
+	jj, err := jwt.ParseString(jwtRaw)
+	if err != nil {
+		return nil, err
+	}
+	// FIXME alg in RS*
+	alg := jj.Header().Algorithm
+	if alg != jwt.RS256 {
+		return nil, fmt.Errorf("bad algo : %s", alg.String())
+	}
+	for _, k := range j.Keys {
+		if k.verifier.Algorithm() == jj.Header().Algorithm {
+			err = k.verifier.Verify(jj.Payload(), jj.Signature())
+			if err != nil {
+				return nil, err
+			}
+			return jj, nil
+		}
+	}
+
+	return nil, fmt.Errorf("can't validate %s", jj.String())
+}
+
 // Auth will ensure JWT token is valid
 func Auth(key string) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
