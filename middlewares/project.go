38f849e890352a079fa38fa85ba1535b52ad1c7b
Mathieu Lecarme <mlecarme@bearstech.com>
2022-01-31 22:13:03 +0100

    Project assert is tested

--- middlewares/project.go
+++ middlewares/project.go
@@ -1,25 +1,44 @@
 package middlewares
 
 import (
-	"context"
 	"net/http"
+	"net/url"
 
 	"github.com/factorysh/microdensity/httpcontext"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 )
 
 // Project adds requested project to context
-func Project() func(next http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			project := chi.URLParam(r, "project")
-			if project == "" {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-
-			ctx := context.WithValue(r.Context(), httpcontext.RequestedProject, project)
-			next.ServeHTTP(w, r.WithContext(ctx))
-		})
-	}
+func AssertProject(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger, _ := zap.NewProduction()
+		l := logger.With(zap.String("url", r.URL.String()))
+		projectRaw := r.Context().Value(httpcontext.RequestedProject)
+		if projectRaw == nil {
+			l.Warn("No project in context")
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+		pp := projectRaw.(string)
+		l = l.With(zap.String("project", pp))
+		if pp == "" {
+			l.Warn("empty project")
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+		p, err := url.QueryUnescape(chi.URLParam(r, "project"))
+		if err != nil || p == "" {
+			l.Error("can't find project in url", zap.Error(err))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		l = l.With(zap.String("project url", p))
+		if pp != p {
+			l.Warn("projects are not equal")
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
