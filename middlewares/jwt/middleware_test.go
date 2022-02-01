efcc1412df288241a2069616c9530e4643542ce3
Mathieu Lecarme <mlecarme@bearstech.com>
2022-02-01 19:41:12 +0100

    More tests, but still broken.

--- middlewares/jwt/middleware_test.go
+++ middlewares/jwt/middleware_test.go
@@ -4,7 +4,9 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
+	"github.com/cristalhq/jwt/v3"
 	"github.com/factorysh/microdensity/mockup"
 	"github.com/stretchr/testify/assert"
 )
@@ -20,8 +22,23 @@ func TestMiddleware(t *testing.T) {
 	})))
 	defer srv.Close()
 
+	client := &http.Client{}
+
 	res, err := http.Get(srv.URL)
 	assert.NoError(t, err)
 	assert.Equal(t, 401, res.StatusCode)
-
+	signer, err := jwt.NewSignerRS(jwt.RS256, privateRSA1024)
+	assert.NoError(t, err)
+	token, err := jwt.NewBuilder(signer,
+		jwt.WithKeyID(mockup.Kid(&privateRSA1024.PublicKey))).Build(
+		jwt.StandardClaims{
+			IssuedAt: jwt.NewNumericDate(time.Now()),
+		})
+	assert.NoError(t, err)
+	r, err := http.NewRequest("GET", srv.URL, nil)
+	assert.NoError(t, err)
+	r.Header.Set("Private-Token", token.String())
+	res, err = client.Do(r)
+	assert.NoError(t, err)
+	assert.Equal(t, 200, res.StatusCode)
 }
