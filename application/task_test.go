b900521945d3f8d4548dd1ba68ee64403623c8db
Wilfried OLLIVIER <wollivier@bearstech.com>
2022-02-02 16:56:10 +0100

    Clean test for tasks

--- application/task_test.go
+++ application/task_test.go
@@ -32,35 +32,48 @@ func TestCreateTask(t *testing.T) {
 	key, app, _, q, cleanUp := prepareTestingContext(t, services)
 	defer cleanUp()
 
-	cli := http.Client{}
-	req, err := mkRequest(key)
-	assert.NoError(t, err)
-	req.Method = "POST"
-	req.URL, err = url.Parse(fmt.Sprintf("%s/service/demo/group%%2Fproject/main/8e54b1d8c5f0859370196733feeb00da022adeb5", app.URL))
-	assert.NoError(t, err)
-	b := &rc{
-		&bytes.Buffer{},
+	tests := []struct {
+		name   string
+		args   map[string]interface{}
+		status int
+		qLen   int
+	}{
+		{name: "Valid args", qLen: 1, args: map[string]interface{}{"HELLO": "Bob"}, status: http.StatusOK}}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			cli := http.Client{}
+			req, err := mkRequest(key)
+			assert.NoError(t, err)
+			req.Method = "POST"
+			req.URL, err = url.Parse(fmt.Sprintf("%s/service/demo/group%%2Fproject/main/8e54b1d8c5f0859370196733feeb00da022adeb5", app.URL))
+			assert.NoError(t, err)
+			b := &rc{
+				&bytes.Buffer{},
+			}
+			err = json.NewEncoder(b).Encode(tc.args)
+			assert.NoError(t, err)
+
+			req.Body = b
+			r, err := cli.Do(req)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.status, r.StatusCode)
+
+			l, err := q.Length()
+			assert.NoError(t, err)
+			assert.Equal(t, tc.qLen, l)
+
+			if tc.status == http.StatusOK {
+				req, err = mkRequest(key)
+				assert.NoError(t, err)
+				req.Method = "GET"
+				req.URL, err = url.Parse(fmt.Sprintf("%s/service/demo/group%%2Fproject/main/8e54b1d8c5f0859370196733feeb00da022adeb5", app.URL))
+				assert.NoError(t, err)
+				r, err = cli.Do(req)
+				assert.NoError(t, err)
+				assert.Equal(t, tc.status, r.StatusCode)
+			}
+		})
 	}
-	err = json.NewEncoder(b).Encode(map[string]interface{}{
-		"HELLO": "Bob",
-	})
-	assert.NoError(t, err)
 
-	req.Body = b
-	r, err := cli.Do(req)
-	assert.NoError(t, err)
-	assert.Equal(t, 200, r.StatusCode)
-
-	l, err := q.Length()
-	assert.NoError(t, err)
-	assert.Equal(t, 1, l)
-
-	req, err = mkRequest(key)
-	assert.NoError(t, err)
-	req.Method = "GET"
-	req.URL, err = url.Parse(fmt.Sprintf("%s/service/demo/group%%2Fproject/main/8e54b1d8c5f0859370196733feeb00da022adeb5", app.URL))
-	assert.NoError(t, err)
-	r, err = cli.Do(req)
-	assert.NoError(t, err)
-	assert.Equal(t, 200, r.StatusCode)
 }
