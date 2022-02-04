d95998c647b85c59f901ed42e693b511ef380e02
Mathieu Lecarme <mlecarme@bearstech.com>
2022-02-04 17:41:14 +0100

    Fix: removing ugly debug.

--- run/compose.go
+++ run/compose.go
@@ -200,15 +200,6 @@ func (c *ComposeRun) PrepareVolumes(prependPath string) error {
 
 // Run a compose service, writing the STDOUT and STDERR outputs, returns the UNIX return code
 func (c *ComposeRun) Run(stdout io.WriteCloser, stderr io.WriteCloser) (int, error) {
-	var err error
-	stderr, err = os.OpenFile("/tmp/microdensity.stderr", os.O_RDWR, 0644)
-	if err != nil {
-		return 0, err
-	}
-	stdout, err = os.OpenFile("/tmp/microdensity.stdout", os.O_RDWR, 0644)
-	if err != nil {
-		return 0, err
-	}
 	c.logger.Info("Run service",
 		zap.String("name", c.project.Name),
 		zap.String("service", c.run),
