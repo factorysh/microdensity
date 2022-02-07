88dfea59d6c86e562b9364b66118e2fa807cf546
Mathieu Lecarme <mlecarme@bearstech.com>
2022-02-07 22:26:47 +0100

    Fix: never display secrets

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -30,7 +31,20 @@ func main() {
 		os.Exit(1)
 	}
 	cfg.Defaults()
-	l = l.With(zap.Any("config", cfg))
+	raw, err := json.Marshal(cfg)
+	if err != nil {
+		l.Error("Marshalling conf", zap.Error(err))
+		os.Exit(1)
+	}
+	var cfgPub conf.Conf
+	err = json.Unmarshal(raw, &cfgPub)
+	if err != nil {
+		l.Error("Marshalling conf", zap.Error(err))
+		os.Exit(1)
+	}
+	cfgPub.OAuth.AppSecret = "•••"
+
+	l = l.With(zap.Any("config", cfgPub))
 
 	a, err := application.New(cfg)
 	if err != nil {
