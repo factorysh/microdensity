1e4d988284d48e9e74353c18b471804d3aab1660
Mathieu Lecarme <mlecarme@bearstech.com>
2022-02-09 09:32:48 +0100

    Badge is done in the container.

--- demo/services/demo/meta.js
+++ demo/services/demo/meta.js
@@ -16,14 +16,3 @@ function validate(params) {
         }
     };
 }
-
-function badge(project, branch, commit, badge) {
-    if (badge != "status") {
-        throw(`Badge unknown ${slug}`);
-    }
-    return {
-        subject: 'status',
-        status: 'bof',
-        color: '#5272B4',
-    };
-}
