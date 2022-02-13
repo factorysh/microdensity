d397de7aabec11988e3433445d5e5414e3e57969
Mathieu Lecarme <mlecarme@bearstech.com>
2022-02-13 22:26:07 +0100

    Cheating with Bulma.

--- application/html.go
+++ application/html.go
@@ -9,15 +9,19 @@ func writeHTMLHeader(w io.Writer) {
 	  <head>
 	    <meta charset="UTF-8">
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<script src="https://cdn.tailwindcss.com"></script>
-		<title>µdensity</title>
+	    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+	    <title>µdensity</title>
 	  </head>
 	  <body>
+	    <section class="section">
+	      <div class="container">
 	`))
 }
 
 func writeHTMLFooter(w io.Writer) {
 	w.Write([]byte(`
+	      </div>
+	    </section>
 	  </body>
 	</html>
 	`))
