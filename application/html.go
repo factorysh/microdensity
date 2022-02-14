6029a973ce57fdc54e6ad5e0dae59034fe7c444b
Wilfried OLLIVIER <wollivier@bearstech.com>
2022-02-14 10:09:29 +0100

    Simplify CSS with just Normalize + Milligram

--- application/html.go
+++ application/html.go
@@ -9,7 +9,8 @@ func writeHTMLHeader(w io.Writer) {
 	  <head>
 	    <meta charset="UTF-8">
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+	    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+	    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
 	    <title>µdensity</title>
 	  </head>
 	  <body>
