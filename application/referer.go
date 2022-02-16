14954dc3d9f39bd83c313d68b2d4f9f0956c59dd
Wilfried OLLIVIER <wollivier@bearstech.com>
2022-02-16 16:16:01 +0100

    Fix: last / in referer urls

--- application/referer.go
+++ application/referer.go
@@ -1,12 +1,14 @@
 package application
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 // RefererMiddleware ensure that requests comes from the gitlab domain
 func (a *Application) RefererMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		referer := r.Referer()
-		if referer != a.GitlabURL {
+		if strings.TrimRight(r.Referer(), "/") != strings.TrimRight(a.GitlabURL, "/") {
 			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 			return
 		}
