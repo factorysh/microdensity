0054edc6f41a1cb7cd854323893e590efbfa1611
Mathieu Lecarme <mlecarme@bearstech.com>
2022-03-02 16:51:39 +0100

    magic happens here.

--- application/logs.go
+++ application/logs.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/docker/docker/pkg/stdcopy"
@@ -12,6 +13,40 @@ import (
 	"github.com/factorysh/microdensity/task"
 	"github.com/robert-nix/ansihtml"
 )
+
+type DoubleLogger struct {
+	Stdout io.Writer
+	Stderr io.Writer
+}
+
+type simpleLogger struct {
+	ouptut io.Writer
+	color  string
+}
+
+func (s *simpleLogger) Write(p []byte) (int, error) {
+	lines := bytes.Split(p, []byte{10})
+	n := 0
+	for _, line := range lines {
+		// FIXME use s.color here
+		i, _ := s.ouptut.Write(line)
+		n += i
+	}
+	return n, nil
+}
+
+func NewDoubleLogger(output io.Writer) *DoubleLogger {
+	return &DoubleLogger{
+		Stdout: &simpleLogger{
+			ouptut: output,
+			color:  "blue",
+		},
+		Stderr: &simpleLogger{
+			ouptut: output,
+			color:  "red",
+		},
+	}
+}
 
 func (a *Application) renderLogsPageForTask(ctx context.Context, t *task.Task, w http.ResponseWriter) error {
 
