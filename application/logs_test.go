6ae9730d45860df60ee846c9c82b56098c524968
Wilfried OLLIVIER <wollivier@bearstech.com>
2022-03-02 18:52:47 +0100

    Check for errors

--- application/logs_test.go
+++ application/logs_test.go
@@ -10,8 +10,10 @@ import (
 func TestDoubleLogger(t *testing.T) {
 	var out bytes.Buffer
 	dl := newDoubleLogger(&out)
-	dl.Stderr.Write([]byte("2022-03-02T14:11:16.733625031Z an error log line"))
-	dl.Stdout.Write([]byte("2022-03-02T14:13:16.733625031Z an stdout log line"))
+	_, err := dl.Stderr.Write([]byte("2022-03-02T14:11:16.733625031Z an error log line"))
+	assert.NoError(t, err)
+	_, err = dl.Stdout.Write([]byte("2022-03-02T14:13:16.733625031Z an stdout log line"))
+	assert.NoError(t, err)
 
 	expexted := `<span class="stderr-prefix">2022-03-02T14:11:16.733625031Z</span> an error log line
 <span class="stdout-prefix">2022-03-02T14:13:16.733625031Z</span> an stdout log line
