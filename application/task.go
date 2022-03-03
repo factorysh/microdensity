95fb0b317eb30dfc54ea5509601f86eab4ed5e39
Wilfried OLLIVIER <wollivier@bearstech.com>
2022-03-03 14:40:03 +0100

    Merge logs file with application/task file

--- application/task.go
+++ application/task.go
@@ -1,7 +1,11 @@
 package application
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
+	"fmt"
+	"html/template"
 	"net/http"
 	"net/url"
 	"os"
@@ -9,10 +13,12 @@ import (
 
 	"github.com/docker/docker/pkg/stdcopy"
 	_claims "github.com/factorysh/microdensity/claims"
+	"github.com/factorysh/microdensity/html"
 	"github.com/factorysh/microdensity/task"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
 	"github.com/google/uuid"
+	"github.com/robert-nix/ansihtml"
 	"go.uber.org/zap"
 )
 
@@ -266,4 +272,35 @@ func (a *Application) TaskLogsHandler(latest bool) func(http.ResponseWriter, *ht
 			return
 		}
 	}
+}
+
+func (a *Application) renderLogsPageForTask(ctx context.Context, t *task.Task, w http.ResponseWriter) error {
+
+	reader, err := t.Logs(ctx, false)
+	if err != nil {
+		return err
+	}
+
+	var buffer bytes.Buffer
+	_, err = stdcopy.StdCopy(&buffer, &buffer, reader)
+	if err != nil {
+		return err
+	}
+
+	data, err := NewTaskPage(t, template.HTML(fmt.Sprintf("<pre>%s</pre>", ansihtml.ConvertToHTML(buffer.Bytes()))), a.GitlabURL, "Task Logs", "terminal")
+	if err != nil {
+		return err
+	}
+
+	p := html.Page{
+		Domain: a.Domain,
+		Detail: fmt.Sprintf("%s / %s - logs", t.Service, t.Commit),
+		Partial: html.Partial{
+			Template: taskTemplate,
+			Data:     data,
+		},
+	}
+
+	w.WriteHeader(http.StatusOK)
+	return p.Render(w)
 }
