1799f950670266aa08b1f6b2f1f0b990539b61f2
Wilfried OLLIVIER <wollivier@bearstech.com>
2022-03-15 16:38:40 +0100

    Add auth to post image handler

--- application/images.go
+++ application/images.go
@@ -1,9 +1,13 @@
 package application
 
 import (
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
@@ -17,15 +21,17 @@ type ImageParams struct {
 	AuthToken string `json:"auth_token"`
 }
 
-// PostImageHandler is used to pull the request image
+// PostImageHandler is used to pull the request image, see https://docs.docker.com/engine/api/sdk/examples/#pull-an-image-with-authentication
 func (a *Application) PostImageHandler(w http.ResponseWriter, r *http.Request) {
 	var imageParams ImageParams
 
 	serviceID := chi.URLParam(r, "serviceID")
 	project := chi.URLParam(r, "project")
+	commit := chi.URLParam(r, "commit")
 	l := a.logger.With(
 		zap.String("service", serviceID),
 		zap.String("project", project),
+		zap.String("commit", commit),
 	)
 
 	d := json.NewDecoder(r.Body)
@@ -36,6 +42,24 @@ func (a *Application) PostImageHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if err := verifyImageName(imageParams.Name, project, commit); err != nil {
+		l.Warn("verifying image name error", zap.Error(err))
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	authConfig := types.AuthConfig{
+		Username: "gitlab-ci-token",
+		Password: imageParams.AuthToken,
+	}
+	encoded, err := json.Marshal(authConfig)
+	if err != nil {
+		l.Warn("encoding auth config", zap.Error(err))
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	auth := base64.URLEncoding.EncodeToString(encoded)
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		l.Warn("new client error", zap.Error(err))
@@ -43,7 +67,7 @@ func (a *Application) PostImageHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	out, err := cli.ImagePull(r.Context(), imageParams.Name, types.ImagePullOptions{RegistryAuth: imageParams.AuthToken})
+	out, err := cli.ImagePull(r.Context(), imageParams.Name, types.ImagePullOptions{RegistryAuth: auth})
 	if err != nil {
 		l.Warn("error when downloading image", zap.Error(err))
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
@@ -61,4 +85,27 @@ func (a *Application) PostImageHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Add("content-type", "text/plain")
 	w.Write(text)
+}
+
+func verifyImageName(name string, project string, commit string) error {
+	parts := strings.Split(name, ":")
+
+	if len(parts) < 2 {
+		return fmt.Errorf("invalid image name %s", name)
+	}
+
+	if parts[len(parts)-1] != commit {
+		return fmt.Errorf("image label name is not equal to commit sha : %s != %s", parts[len(parts)-1], commit)
+	}
+
+	unescaped, err := url.PathUnescape(project)
+	if err != nil {
+		return err
+	}
+
+	if !strings.Contains(name, unescaped) {
+		return fmt.Errorf("image %s do not match project %s", name, unescaped)
+	}
+
+	return nil
 }
