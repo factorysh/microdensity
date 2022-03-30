53d1bbfc18cbd2c8d6727354ba59bd5596dc7a4e
Wilfried OLLIVIER <wollivier@bearstech.com>
2022-03-30 14:45:01 +0200

    Fix: add creation date

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -12,11 +12,12 @@ import (
 )
 
 var dummyTask = &task.Task{
-	Id:      uuid.New(),
-	Service: "demo",
-	Project: "group%20project",
-	Branch:  "main",
-	Commit:  "01279848527693d126de60ec7b355924c96d2957",
+	Id:       uuid.New(),
+	Service:  "demo",
+	Project:  "group%20project",
+	Branch:   "main",
+	Commit:   "01279848527693d126de60ec7b355924c96d2957",
+	Creation: time.Now(),
 }
 
 const defaultTestDir = "/tmp/microdensity/data"
